4b80b266c8860311beb08ce4ec492b22910539ab
Alex Dumouchelle <dumoucat@dukes.jmu.edu>
2022-11-28 15:56:30 -0500

    started using JS to print calendar. Almost done implementing data structure

--- scripts.js
+++ scripts.js
@@ -2,3 +2,13 @@
 function validTime(start, end) {
   return start < end;
 }
+
+
+
+function addEvent(hour = 12, name = "event") {
+  const event = document.createElement("div");
+  event.className = "col-2";
+  const eventName = document.createTextNode(name);
+  event.appendChild(eventName);
+  document.getElementById("" + hour).appendChild(event);
+}
