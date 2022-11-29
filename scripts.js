9284e017d02905654c38987cee11e989d99b59e9
Alex Dumouchelle <dumoucat@dukes.jmu.edu>
2022-11-28 19:37:09 -0500

    adding events to the myCal works. Need to figure out how to edit and remove

--- scripts.js
+++ scripts.js
@@ -3,12 +3,40 @@ function validTime(start, end) {
   return start < end;
 }
 
+// Creates a calendar node for the DOM at from start hour to end hour (inclusive)
+// Input is in 24ht time, displays 12hr time
+function createCalendar(start = 8, end = 20) {
+  const container = document.createElement("section");
+  container.className = "container";
 
+  // builds the calendar graphic
+  for (var i = start; i <= end; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    row.id = i;
+    container.appendChild(row);
 
-function addEvent(hour = 12, name = "event") {
+    const time = document.createElement("div");
+    time.className = "row";
+    // Converts 24hr time to 12hr time
+    const text = document.createTextNode(
+      (i < 12) ? (i + " am") : (((i % 12 == 0) ? 12 : i % 12) + " pm"));
+    time.className = "col-2";
+
+    time.appendChild(text);
+    row.appendChild(time);
+  }
+  // document.getElementById("container").appendChild(container);
+  return container;
+}
+
+// Adds an event to the DOM at the hour given.
+// Will eventually have support for events longer than an hours
+function addEvent(start = 12, end = 13, name = "event") {
   const event = document.createElement("div");
   event.className = "col-2";
   const eventName = document.createTextNode(name);
   event.appendChild(eventName);
-  document.getElementById("" + hour).appendChild(event);
+  // TODO, set up multi hour events
+  return event;
 }
